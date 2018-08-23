ad6e7b0638e69b87fa734dab4d217a38f453a38a
Nasser Sanou <nassersanou23@gmail.com>
2018-08-23 00:59:22 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,10 +5,3 @@ require 'pry'
 # end
 
 # # call your method here!
-
-def yielding
-  puts "the program is executing the code inside the method"
-  yield { puts "the method has yielded to the block!" }
-  puts "now we are back in the method"
-end
-puts yielding
