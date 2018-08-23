dfefc9cf308e22c4647c3adde806236d2169caf6
Nasser Sanou <nassersanou23@gmail.com>
2018-08-23 01:22:49 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,13 +3,13 @@
 # array = ["Tim", "Tom", "Jim"]
 def hello_t(array)
   if block_given? 
-
   i = 0 
+  
   while i < array.length 
-  yield array[i]
- 
-  i += 1 
+    yield array[i]
+    i += 1 
   end
+  
   else 
    puts "Hey! No block was given!"
   end 
